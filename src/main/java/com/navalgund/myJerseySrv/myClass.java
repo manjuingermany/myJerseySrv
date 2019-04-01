568538d5ff83b7060652072cce4df52823ff1d85
Manjunath Navalgund <Manjunath.Navalgund@gmail.com>
2019-04-01 20:49:07 +0530

    updated from eclipse

--- src/main/java/com/navalgund/myJerseySrv/myClass.java
+++ src/main/java/com/navalgund/myJerseySrv/myClass.java
@@ -12,7 +12,7 @@ public class myClass {
 	@GET //GET tells Jersey to call this method to service a GET HTTP request on the class’s URL
 	@Produces(MediaType.TEXT_HTML) //Produces says what HTTP media type this method returns. 
 	public String sayHtmlHello() {
-	return "Hello from Jerseyyyy! this updated from github ";
+	return "Hello from Jerseyyyy! this updated from github; this is updated from eclipse ";
 	}
 
 }
