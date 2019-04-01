b75805b3b89eabfbfa582950a4847b25a3ad501e
Manjunath Navalgund <Manjunath.Navalgund@gmail.com>
2019-04-01 20:26:31 +0530

    initial version

---
+++ src/main/java/com/navalgund/myJerseySrv/myClass.java
@@ -0,0 +1,18 @@
+
+package com.navalgund.myJerseySrv;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("hello") //Path tells Jersey what URL subtree this class responds to
+
+public class myClass {
+	@GET //GET tells Jersey to call this method to service a GET HTTP request on the class’s URL
+	@Produces(MediaType.TEXT_HTML) //Produces says what HTTP media type this method returns. 
+	public String sayHtmlHello() {
+	return "Hello from Jerseyyyy!";
+	}
+
+}
